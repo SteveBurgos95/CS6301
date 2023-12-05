4660674b4c38d5baab89f1bf35ed376bfe02e3cb
Jason <77454384+kehngid@users.noreply.github.com>
2023-12-05 00:16:29 -0600

    Update model.py

--- MomentumRNN/model.py
+++ MomentumRNN/model.py
@@ -26,32 +26,3 @@ class MomentumLSTM(nn.Module):
             output = self.fc(x)
             outputs.append(output)
         return torch.stack(outputs), hidden_tuple, v
-
-'''
-# Example usage:
-input_size = 10
-hidden_size = 20
-output_size = 1
-seq_length = 5
-batch_size = 32
-
-# Create an instance of the PredictiveLSTM model
-model = PredictiveLSTM(input_size, hidden_size, output_size)
-
-# Dummy input sequence
-input_seq = torch.randn(seq_length, batch_size, input_size)
-
-# Initial hidden state and momentum state
-initial_hidden = (
-    torch.zeros(batch_size, hidden_size),
-    torch.zeros(batch_size, hidden_size)
-)
-initial_momentum = torch.zeros(batch_size, hidden_size)
-
-# Forward pass
-output_seq, final_hidden, final_momentum = model(input_seq, initial_hidden, initial_momentum)
-
-print("Output sequence shape:", output_seq.shape)
-print("Final hidden state shape:", final_hidden[0].shape)
-print("Final momentum state shape:", final_momentum.shape)
-'''
